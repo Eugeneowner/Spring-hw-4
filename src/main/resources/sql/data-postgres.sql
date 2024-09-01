e5275f87a4d7b8da88cc1cc277cdfd34ded10cd8
Eugeneowner <ownereugene@gmail.com>
2024-09-01 16:27:33 +0300

    HW 4 fix 2

--- src/main/resources/sql/data-postgres.sql
+++ src/main/resources/sql/data-postgres.sql
@@ -6,14 +6,15 @@ INSERT INTO roles(role_id, role_name, user_id)
 VALUES (101, 'USER', 1),
        (102, 'ADMIN', 2);
 
+
 INSERT INTO customers (name, email, age, password, phone, created_date, last_modified_date)
 VALUES ('Eugene', 'eugene@gmail.com', 38, 'qWerty', '+380672222222', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        ('Tanya', 'tanya@gmail.com', 30, 'qWerty', '+380631113311', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        ('Nikita', 'nikita@gmail.com', 37, 'qWerty', '+380933332233', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('Sergey', 'sergey@gmail.com', 42, 'qWerty', '+1987654321', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('Alexey', 'alexey@gmail.com', 44, 'qWerty', '+1092847563', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('Aleksandr', 'alexander@gmail.com', 28, 'qWerty', '+1234509876', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('Anna', 'anna@gmail.com', 31, 'qWerty', '+1029384756', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Sergey', 'sergey@gmail.com', 42, 'qWerty', '+380933332211', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Alexey', 'alexey@gmail.com', 44, 'qWerty', '+380666332233', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Aleksandr', 'alexander@gmail.com', 28, 'qWerty', '++380933332443', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Anna', 'anna@gmail.com', 31, 'qWerty', '+380933334444', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 
 
 INSERT INTO accounts (number, currency, balance, customer_id, created_date, last_modified_date)
